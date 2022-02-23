7f4c059360f00c6ce3f8c2204d9d05928d1c01bb
Tirth Diwani <tirth99092@gmail.com>
2022-02-23 13:34:44 +0530

    Add files via upload

---
+++ MallStockMod/Stock_Available.py
@@ -0,0 +1,13 @@
+def available(x, y):
+    print('Total Available Stock')
+    print(x, ':', y)
+
+
+def item_show(x, y,z):
+    print('Selling prices and Arriving prices for each product')
+    print(x,':', y, ':',z)
+
+
+def total_commission(x, y):
+    print('Total Commission to sales person on each product')
+    print(x, ':', y)
